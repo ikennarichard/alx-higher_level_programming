c0d7e56193742230507146e120fac5c2ad264fbe
ikennarichard <oguejioforrichard@gmail.com>
2022-09-21 08:47:40 +0000

    Addprogram for project

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-	if chr(i) != 'q' and chr(i) != 'e':
-		print("{}".format(chr(i)), end="")
+for char in range(26):
+    if char != 4 and char != 16:
+        print("{:s}".format(chr(char + ord("a"))), end="")
